6be6f7d417ba51f8ecb468785c3eb699dad018b2
Mnde81 <minubis@gmail.com>
2024-09-10 08:44:44 +0300

    updated file

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import { apiRouter } from './router/api.js';
 
 const app = express();
-
 const port = 5020;
 
 app.use('/api', apiRouter);
@@ -12,17 +11,16 @@ app.all('*', (req, res) => {
         status: 'error',
         msg: 'Ne ten pataikei :P',
     });
-
 });
 
-// app.use((err, req, res, next) => {
-//     console.error(err.stack)
-//     res.status(500).send('Something broke!')
-//   });
+app.use((req, res, next) => {
+    return res.status(404).send("Sorry can't find that!");
+});
 
-// app.use((req, res, next) => {
-//     res.status(404).send("Sorry can't find that!")
-//   });
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    return res.status(500).send('Something broke!');
+});
 
 app.listen(port, () => {
     console.log('Turizmo serveris: http://localhost:' + port);
